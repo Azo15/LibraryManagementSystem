a98f7981f15fa360f51d387c040c075d5797cdde
Azo Ismail <ismailazo260@gmail.com>
2026-04-09 15:11:50 +0300

    feat: add ReportsForm UI with statistics dashboard and popular books grid

--- Forms/ReportsForm.Designer.cs
+++ Forms/ReportsForm.Designer.cs
@@ -5,6 +5,7 @@ namespace LibraryManagementSystem.Forms
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel pnlStats;
         private System.Windows.Forms.Label lblDailyBorrows;
         private System.Windows.Forms.Label lblDailyReturns;
         private System.Windows.Forms.Label lblWeeklyBorrows;
@@ -29,6 +30,7 @@ namespace LibraryManagementSystem.Forms
             this.btnRefresh = new System.Windows.Forms.Button();
             this.dgvMostBorrowed = new System.Windows.Forms.DataGridView();
             this.lblMostBorrowed = new System.Windows.Forms.Label();
+            this.pnlStats = new System.Windows.Forms.Panel();
             this.lblOverdue = new System.Windows.Forms.Label();
             this.lblMonthlyBorrows = new System.Windows.Forms.Label();
             this.lblWeeklyBorrows = new System.Windows.Forms.Label();
@@ -36,19 +38,17 @@ namespace LibraryManagementSystem.Forms
             this.lblDailyBorrows = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
             this.panelMain.SuspendLayout();
+            this.pnlStats.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMostBorrowed)).BeginInit();
             this.SuspendLayout();
+            
             // 
             // panelMain
             // 
             this.panelMain.Controls.Add(this.btnRefresh);
             this.panelMain.Controls.Add(this.dgvMostBorrowed);
             this.panelMain.Controls.Add(this.lblMostBorrowed);
-            this.panelMain.Controls.Add(this.lblOverdue);
-            this.panelMain.Controls.Add(this.lblMonthlyBorrows);
-            this.panelMain.Controls.Add(this.lblWeeklyBorrows);
-            this.panelMain.Controls.Add(this.lblDailyReturns);
-            this.panelMain.Controls.Add(this.lblDailyBorrows);
+            this.panelMain.Controls.Add(this.pnlStats);
             this.panelMain.Controls.Add(this.lblTitle);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Location = new System.Drawing.Point(0, 0);
@@ -56,117 +56,143 @@ namespace LibraryManagementSystem.Forms
             this.panelMain.Padding = new System.Windows.Forms.Padding(20);
             this.panelMain.Size = new System.Drawing.Size(1000, 600);
             this.panelMain.TabIndex = 0;
+            
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.lblTitle.Location = new System.Drawing.Point(30, 20);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(200, 24);
+            this.lblTitle.Size = new System.Drawing.Size(200, 30);
             this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Raporlar ve Analizler";
+            this.lblTitle.Text = "Analizler ve Raporlar";
+            
+            // 
+            // pnlStats
+            // 
+            this.pnlStats.BackColor = System.Drawing.Color.White;
+            this.pnlStats.Controls.Add(this.lblDailyBorrows);
+            this.pnlStats.Controls.Add(this.lblDailyReturns);
+            this.pnlStats.Controls.Add(this.lblWeeklyBorrows);
+            this.pnlStats.Controls.Add(this.lblMonthlyBorrows);
+            this.pnlStats.Controls.Add(this.lblOverdue);
+            this.pnlStats.Location = new System.Drawing.Point(30, 70);
+            this.pnlStats.Name = "pnlStats";
+            this.pnlStats.Size = new System.Drawing.Size(940, 80);
+            this.pnlStats.TabIndex = 1;
+            
             // 
             // lblDailyBorrows
             // 
             this.lblDailyBorrows.AutoSize = true;
-            this.lblDailyBorrows.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblDailyBorrows.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
-            this.lblDailyBorrows.Location = new System.Drawing.Point(30, 70);
+            this.lblDailyBorrows.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDailyBorrows.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.lblDailyBorrows.Location = new System.Drawing.Point(20, 30);
             this.lblDailyBorrows.Name = "lblDailyBorrows";
-            this.lblDailyBorrows.Size = new System.Drawing.Size(130, 18);
+            this.lblDailyBorrows.Size = new System.Drawing.Size(130, 21);
             this.lblDailyBorrows.TabIndex = 1;
             this.lblDailyBorrows.Text = "Günlük Ödünç: 0";
+            
             // 
             // lblDailyReturns
             // 
             this.lblDailyReturns.AutoSize = true;
-            this.lblDailyReturns.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblDailyReturns.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
-            this.lblDailyReturns.Location = new System.Drawing.Point(200, 70);
+            this.lblDailyReturns.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDailyReturns.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.lblDailyReturns.Location = new System.Drawing.Point(200, 30);
             this.lblDailyReturns.Name = "lblDailyReturns";
-            this.lblDailyReturns.Size = new System.Drawing.Size(110, 18);
+            this.lblDailyReturns.Size = new System.Drawing.Size(110, 21);
             this.lblDailyReturns.TabIndex = 2;
             this.lblDailyReturns.Text = "Günlük İade: 0";
+            
             // 
             // lblWeeklyBorrows
             // 
             this.lblWeeklyBorrows.AutoSize = true;
-            this.lblWeeklyBorrows.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblWeeklyBorrows.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(89)))), ((int)(((byte)(182)))));
-            this.lblWeeklyBorrows.Location = new System.Drawing.Point(370, 70);
+            this.lblWeeklyBorrows.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblWeeklyBorrows.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.lblWeeklyBorrows.Location = new System.Drawing.Point(370, 30);
             this.lblWeeklyBorrows.Name = "lblWeeklyBorrows";
-            this.lblWeeklyBorrows.Size = new System.Drawing.Size(140, 18);
+            this.lblWeeklyBorrows.Size = new System.Drawing.Size(140, 21);
             this.lblWeeklyBorrows.TabIndex = 3;
             this.lblWeeklyBorrows.Text = "Haftalık Ödünç: 0";
+            
             // 
             // lblMonthlyBorrows
             // 
             this.lblMonthlyBorrows.AutoSize = true;
-            this.lblMonthlyBorrows.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblMonthlyBorrows.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
-            this.lblMonthlyBorrows.Location = new System.Drawing.Point(550, 70);
+            this.lblMonthlyBorrows.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMonthlyBorrows.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.lblMonthlyBorrows.Location = new System.Drawing.Point(550, 30);
             this.lblMonthlyBorrows.Name = "lblMonthlyBorrows";
-            this.lblMonthlyBorrows.Size = new System.Drawing.Size(120, 18);
+            this.lblMonthlyBorrows.Size = new System.Drawing.Size(120, 21);
             this.lblMonthlyBorrows.TabIndex = 4;
             this.lblMonthlyBorrows.Text = "Aylık Ödünç: 0";
+            
             // 
             // lblOverdue
             // 
             this.lblOverdue.AutoSize = true;
-            this.lblOverdue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblOverdue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
-            this.lblOverdue.Location = new System.Drawing.Point(720, 70);
+            this.lblOverdue.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOverdue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.lblOverdue.Location = new System.Drawing.Point(740, 30);
             this.lblOverdue.Name = "lblOverdue";
-            this.lblOverdue.Size = new System.Drawing.Size(160, 18);
+            this.lblOverdue.Size = new System.Drawing.Size(160, 21);
             this.lblOverdue.TabIndex = 5;
             this.lblOverdue.Text = "Gecikmiş Kitaplar: 0";
+            
             // 
             // lblMostBorrowed
             // 
             this.lblMostBorrowed.AutoSize = true;
-            this.lblMostBorrowed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblMostBorrowed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
-            this.lblMostBorrowed.Location = new System.Drawing.Point(30, 120);
+            this.lblMostBorrowed.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMostBorrowed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.lblMostBorrowed.Location = new System.Drawing.Point(30, 180);
             this.lblMostBorrowed.Name = "lblMostBorrowed";
-            this.lblMostBorrowed.Size = new System.Drawing.Size(220, 20);
+            this.lblMostBorrowed.Size = new System.Drawing.Size(220, 25);
             this.lblMostBorrowed.TabIndex = 6;
-            this.lblMostBorrowed.Text = "En Çok Ödünç Alınan Kitaplar";
+            this.lblMostBorrowed.Text = "Popüler Kitaplar Listesi";
+            
             // 
             // dgvMostBorrowed
             // 
             this.dgvMostBorrowed.AllowUserToAddRows = false;
             this.dgvMostBorrowed.AllowUserToDeleteRows = false;
+            this.dgvMostBorrowed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvMostBorrowed.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvMostBorrowed.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMostBorrowed.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Title", HeaderText = "Başlık" },
-                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Author", HeaderText = "Yazar" },
-                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "BorrowCount", HeaderText = "Ödünç Sayısı" }
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Title", HeaderText = "Kitap Başlığı" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Author", HeaderText = "Yazar Bilgisi" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "BorrowCount", HeaderText = "Ödünç Alınma Sayısı" }
             });
-            this.dgvMostBorrowed.Location = new System.Drawing.Point(30, 150);
+            this.dgvMostBorrowed.Location = new System.Drawing.Point(30, 220);
             this.dgvMostBorrowed.MultiSelect = false;
             this.dgvMostBorrowed.Name = "dgvMostBorrowed";
             this.dgvMostBorrowed.ReadOnly = true;
             this.dgvMostBorrowed.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvMostBorrowed.Size = new System.Drawing.Size(940, 380);
+            this.dgvMostBorrowed.Size = new System.Drawing.Size(940, 310);
             this.dgvMostBorrowed.TabIndex = 7;
+            
             // 
             // btnRefresh
             // 
-            this.btnRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
             this.btnRefresh.FlatAppearance.BorderSize = 0;
             this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnRefresh.ForeColor = System.Drawing.Color.White;
-            this.btnRefresh.Location = new System.Drawing.Point(840, 550);
+            this.btnRefresh.Location = new System.Drawing.Point(840, 545);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(130, 35);
             this.btnRefresh.TabIndex = 8;
-            this.btnRefresh.Text = "Yenile";
+            this.btnRefresh.Text = "Güncelle";
             this.btnRefresh.UseVisualStyleBackColor = false;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            
             // 
             // ReportsForm
             // 
@@ -181,9 +207,10 @@ namespace LibraryManagementSystem.Forms
             this.Text = "Raporlar";
             this.panelMain.ResumeLayout(false);
             this.panelMain.PerformLayout();
+            this.pnlStats.ResumeLayout(false);
+            this.pnlStats.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMostBorrowed)).EndInit();
             this.ResumeLayout(false);
         }
     }
 }
-
