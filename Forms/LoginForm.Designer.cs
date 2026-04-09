dcbfb7ec75705a7e8d769aabac03f4a35d62c1a0
Azo Ismail <ismailazo260@gmail.com>
2026-04-09 15:06:46 +0300

    feat: implement UI layout for LoginForm with email, password, and registration controls

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -3,12 +3,17 @@ namespace LibraryManagementSystem.Forms
     partial class LoginForm
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Label lblBrand;
+        private System.Windows.Forms.Label lblBrandSub;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Panel pnlEmailLine;
         private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Panel pnlPasswordLine;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Label lblRegister;
@@ -24,97 +29,175 @@ namespace LibraryManagementSystem.Forms
 
         private void InitializeComponent()
         {
-            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.lblBrandSub = new System.Windows.Forms.Label();
+            this.lblBrand = new System.Windows.Forms.Label();
+            this.panelRight = new System.Windows.Forms.Panel();
             this.lblRegister = new System.Windows.Forms.Label();
             this.btnRegister = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.pnlPasswordLine = new System.Windows.Forms.Panel();
             this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
+            this.pnlEmailLine = new System.Windows.Forms.Panel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.lblEmail = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.panelMain.SuspendLayout();
+            
+            this.panelLeft.SuspendLayout();
+            this.panelRight.SuspendLayout();
             this.SuspendLayout();
+            
             // 
-            // panelMain
+            // panelLeft
             // 
-            this.panelMain.Controls.Add(this.lblRegister);
-            this.panelMain.Controls.Add(this.btnRegister);
-            this.panelMain.Controls.Add(this.btnLogin);
-            this.panelMain.Controls.Add(this.txtPassword);
-            this.panelMain.Controls.Add(this.txtEmail);
-            this.panelMain.Controls.Add(this.lblPassword);
-            this.panelMain.Controls.Add(this.lblEmail);
-            this.panelMain.Controls.Add(this.lblTitle);
-            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMain.Location = new System.Drawing.Point(0, 0);
-            this.panelMain.Name = "panelMain";
-            this.panelMain.Padding = new System.Windows.Forms.Padding(40);
-            this.panelMain.Size = new System.Drawing.Size(450, 400);
-            this.panelMain.TabIndex = 0;
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panelLeft.Controls.Add(this.lblBrandSub);
+            this.panelLeft.Controls.Add(this.lblBrand);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(350, 500);
+            this.panelLeft.TabIndex = 0;
+            
+            // 
+            // lblBrand
+            // 
+            this.lblBrand.AutoSize = true;
+            this.lblBrand.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBrand.ForeColor = System.Drawing.Color.White;
+            this.lblBrand.Location = new System.Drawing.Point(50, 180);
+            this.lblBrand.Name = "lblBrand";
+            this.lblBrand.Size = new System.Drawing.Size(250, 45);
+            this.lblBrand.TabIndex = 0;
+            this.lblBrand.Text = "KYS PROJESİ";
+            
+            // 
+            // lblBrandSub
+            // 
+            this.lblBrandSub.AutoSize = true;
+            this.lblBrandSub.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBrandSub.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.lblBrandSub.Location = new System.Drawing.Point(60, 235);
+            this.lblBrandSub.Name = "lblBrandSub";
+            this.lblBrandSub.Size = new System.Drawing.Size(200, 21);
+            this.lblBrandSub.TabIndex = 1;
+            this.lblBrandSub.Text = "Modern Kütüphane Sistemi";
+            
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.White;
+            this.panelRight.Controls.Add(this.lblRegister);
+            this.panelRight.Controls.Add(this.btnRegister);
+            this.panelRight.Controls.Add(this.btnLogin);
+            this.panelRight.Controls.Add(this.pnlPasswordLine);
+            this.panelRight.Controls.Add(this.txtPassword);
+            this.panelRight.Controls.Add(this.lblPassword);
+            this.panelRight.Controls.Add(this.pnlEmailLine);
+            this.panelRight.Controls.Add(this.txtEmail);
+            this.panelRight.Controls.Add(this.lblEmail);
+            this.panelRight.Controls.Add(this.lblTitle);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight.Location = new System.Drawing.Point(350, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(450, 500);
+            this.panelRight.TabIndex = 1;
+            
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
-            this.lblTitle.Location = new System.Drawing.Point(100, 40);
+            this.lblTitle.Location = new System.Drawing.Point(50, 60);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(250, 29);
+            this.lblTitle.Size = new System.Drawing.Size(180, 37);
             this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Kütüphane Yönetimi";
+            this.lblTitle.Text = "Hoş Geldiniz";
+            
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblEmail.Location = new System.Drawing.Point(50, 120);
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEmail.ForeColor = System.Drawing.Color.Gray;
+            this.lblEmail.Location = new System.Drawing.Point(50, 150);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(54, 17);
+            this.lblEmail.Size = new System.Drawing.Size(64, 19);
             this.lblEmail.TabIndex = 1;
-            this.lblEmail.Text = "E-posta:";
+            this.lblEmail.Text = "E-POSTA";
+            
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.txtEmail.Location = new System.Drawing.Point(50, 180);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(350, 22);
+            this.txtEmail.TabIndex = 2;
+            
+            // 
+            // pnlEmailLine
+            // 
+            this.pnlEmailLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.pnlEmailLine.Location = new System.Drawing.Point(50, 205);
+            this.pnlEmailLine.Name = "pnlEmailLine";
+            this.pnlEmailLine.Size = new System.Drawing.Size(350, 2);
+            this.pnlEmailLine.TabIndex = 3;
+            
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblPassword.Location = new System.Drawing.Point(50, 180);
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Gray;
+            this.lblPassword.Location = new System.Drawing.Point(50, 240);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(41, 17);
-            this.lblPassword.TabIndex = 2;
-            this.lblPassword.Text = "Şifre:";
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtEmail.Location = new System.Drawing.Point(50, 145);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(350, 23);
-            this.txtEmail.TabIndex = 3;
+            this.lblPassword.Size = new System.Drawing.Size(46, 19);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "ŞİFRE";
+            
             // 
             // txtPassword
             // 
-            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtPassword.Location = new System.Drawing.Point(50, 205);
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.txtPassword.Location = new System.Drawing.Point(50, 270);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(350, 23);
-            this.txtPassword.TabIndex = 4;
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.Size = new System.Drawing.Size(350, 22);
+            this.txtPassword.TabIndex = 5;
+            
+            // 
+            // pnlPasswordLine
+            // 
+            this.pnlPasswordLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.pnlPasswordLine.Location = new System.Drawing.Point(50, 295);
+            this.pnlPasswordLine.Name = "pnlPasswordLine";
+            this.pnlPasswordLine.Size = new System.Drawing.Size(350, 2);
+            this.pnlPasswordLine.TabIndex = 6;
+            
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
             this.btnLogin.FlatAppearance.BorderSize = 0;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(50, 260);
+            this.btnLogin.Location = new System.Drawing.Point(50, 340);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(350, 40);
-            this.btnLogin.TabIndex = 5;
-            this.btnLogin.Text = "Giriş Yap";
+            this.btnLogin.Size = new System.Drawing.Size(350, 45);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "GİRİŞ YAP";
             this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            
             // 
             // btnRegister
             // 
@@ -122,45 +205,49 @@ namespace LibraryManagementSystem.Forms
             this.btnRegister.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
             this.btnRegister.FlatAppearance.BorderSize = 2;
             this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
-            this.btnRegister.Location = new System.Drawing.Point(50, 320);
+            this.btnRegister.Location = new System.Drawing.Point(50, 420);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(350, 35);
-            this.btnRegister.TabIndex = 6;
-            this.btnRegister.Text = "Kayıt Ol";
+            this.btnRegister.Size = new System.Drawing.Size(350, 45);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "KAYIT OL";
             this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            
             // 
             // lblRegister
             // 
             this.lblRegister.AutoSize = true;
-            this.lblRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRegister.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblRegister.ForeColor = System.Drawing.Color.Gray;
-            this.lblRegister.Location = new System.Drawing.Point(150, 305);
+            this.lblRegister.Location = new System.Drawing.Point(165, 400);
             this.lblRegister.Name = "lblRegister";
-            this.lblRegister.Size = new System.Drawing.Size(150, 13);
-            this.lblRegister.TabIndex = 7;
+            this.lblRegister.Size = new System.Drawing.Size(120, 17);
+            this.lblRegister.TabIndex = 9;
             this.lblRegister.Text = "Hesabınız yok mu?";
             this.lblRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 400);
-            this.Controls.Add(this.panelMain);
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Giriş";
+            this.Text = "Giriş Ekranı";
             this.Load += new System.EventHandler(this.LoginForm_Load);
-            this.panelMain.ResumeLayout(false);
-            this.panelMain.PerformLayout();
+            this.panelLeft.ResumeLayout(false);
+            this.panelLeft.PerformLayout();
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
             this.ResumeLayout(false);
         }
     }
 }
-
