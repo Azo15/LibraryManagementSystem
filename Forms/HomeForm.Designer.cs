3841e1c56bb7f59a2da037eef69f613d77a11863
Azo Ismail <ismailazo260@gmail.com>
2026-04-14 01:20:47 +0300

    feat: initialize HomeForm UI with dashboard banner, statistics cards, and charts

--- Forms/HomeForm.Designer.cs
+++ Forms/HomeForm.Designer.cs
@@ -26,6 +26,11 @@ namespace LibraryManagementSystem.Forms
         private System.Windows.Forms.DataVisualization.Charting.Chart chartPie;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartLine;
 
+        // Side Feed Panel
+        private System.Windows.Forms.Panel pnlSideFeed;
+        private System.Windows.Forms.Label lblSideFeedTitle;
+        private System.Windows.Forms.FlowLayoutPanel flpActivities;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -62,11 +67,16 @@ namespace LibraryManagementSystem.Forms
 
             this.chartPie = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartLine = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            
+            this.pnlSideFeed = new System.Windows.Forms.Panel();
+            this.lblSideFeedTitle = new System.Windows.Forms.Label();
+            this.flpActivities = new System.Windows.Forms.FlowLayoutPanel();
 
             this.pnlCard1.SuspendLayout();
             this.pnlCard2.SuspendLayout();
             this.pnlCard3.SuspendLayout();
             this.pnlCard4.SuspendLayout();
+            this.pnlSideFeed.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartPie)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartLine)).BeginInit();
             this.SuspendLayout();
@@ -214,13 +224,52 @@ namespace LibraryManagementSystem.Forms
             this.chartLine.TabIndex = 6;
             this.chartLine.Text = "Son 6 Ayın Ödünç Alma Trendi";
             this.chartLine.Titles.Add(new System.Windows.Forms.DataVisualization.Charting.Title("Son 6 Ayın Ödünç Alma Trendi", System.Windows.Forms.DataVisualization.Charting.Docking.Top, new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold), System.Drawing.Color.FromArgb(44, 62, 80)));
+            // Make charts stretch slightly if needed, but fixed size is usually safer.
+            this.chartLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.chartPie.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Bottom)));
+
+            // 
+            // pnlSideFeed
+            // 
+            this.pnlSideFeed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlSideFeed.Location = new System.Drawing.Point(1000, 190);
+            this.pnlSideFeed.Name = "pnlSideFeed";
+            this.pnlSideFeed.Size = new System.Drawing.Size(470, 470);
+            this.pnlSideFeed.TabIndex = 7;
+            this.pnlSideFeed.Paint += new System.Windows.Forms.PaintEventHandler(this.CardPanel_Paint);
+            this.pnlSideFeed.Controls.Add(this.lblSideFeedTitle);
+            this.pnlSideFeed.Controls.Add(this.flpActivities);
+
+            // 
+            // lblSideFeedTitle
+            // 
+            this.lblSideFeedTitle.AutoSize = true;
+            this.lblSideFeedTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblSideFeedTitle.Text = "SON İŞLEMLER & BİLDİRİMLER";
+            this.lblSideFeedTitle.ForeColor = System.Drawing.Color.Gray;
+            this.lblSideFeedTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblSideFeedTitle.BackColor = System.Drawing.Color.White;
+
+            // 
+            // flpActivities
+            // 
+            this.flpActivities.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.flpActivities.Location = new System.Drawing.Point(20, 50);
+            this.flpActivities.Name = "flpActivities";
+            this.flpActivities.Size = new System.Drawing.Size(430, 400);
+            this.flpActivities.TabIndex = 8;
+            this.flpActivities.BackColor = System.Drawing.Color.White;
+            this.flpActivities.AutoScroll = true;
+            this.flpActivities.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flpActivities.WrapContents = false;
 
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 700);
+            this.ClientSize = new System.Drawing.Size(1500, 700);
+            this.Controls.Add(this.pnlSideFeed);
             this.Controls.Add(this.chartLine);
             this.Controls.Add(this.chartPie);
             this.Controls.Add(this.pnlCard4);
@@ -240,6 +289,8 @@ namespace LibraryManagementSystem.Forms
             this.pnlCard3.PerformLayout();
             this.pnlCard4.ResumeLayout(false);
             this.pnlCard4.PerformLayout();
+            this.pnlSideFeed.ResumeLayout(false);
+            this.pnlSideFeed.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartPie)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartLine)).EndInit();
             this.ResumeLayout(false);
